acec8a7f811d3242afd86b835d784d073dbfc86d
AsliddinWeb <asliddinsinger@gmail.com>
2024-04-25 15:02:15 +0500

    Permissions deleted

--- course_app/views.py
+++ course_app/views.py
@@ -6,20 +6,20 @@ from .filters import YonalishFilter
 class FilialListView(generics.ListAPIView):
     queryset = Filial.objects.all()
     serializer_class = FilialSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 class TalimTuriListView(generics.ListAPIView):
     queryset = TalimTuri.objects.all()
     serializer_class = TalimTuriSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 class TalimShakliListView(generics.ListAPIView):
     queryset = TalimShakli.objects.all()
     serializer_class = TalimShakliSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 class YonalishListView(generics.ListAPIView):
     queryset = Yonalish.objects.all()
     serializer_class = YonalishSerializer
     filter_class = YonalishFilter
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
